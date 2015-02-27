6f90adc9e1d7e644fedd6e4f99240835a6f03af0
Sunil Sadasivan <sunil@bufferapp.com>
2015-02-27 17:48:58 -0500

    Initial logic around ensuring we don't poll when there are no clients

--- server.js
+++ server.js
@@ -4,18 +4,34 @@ var express = require('express.io'),
 app.http().io()
 
 interval = null;
+var clients = 0;
 app.io.route('ready', function(req) {
   var ec2 = require('./handlers/ec2');
   ec2.list(app);
-  if(!interval) {
-    interval = setInterval(function(){ec2.list(app);}, 1000*10);
+  if(clients == 0) {
+    poll = setInterval(function(){ec2.list(app);}, 1000*10);
   }
+  clients++;
+
   req.io.emit('talk', {
     message: 'io event from an io route on the server'
   });
+  console.log('ENTER: number of clients ' + clients);
+})
+
+app.io.route('exit', function(req) {
+  var ec2 = require('./handlers/ec2');
+  ec2.list(app);
+  if(clients > 0) clients--;
+  if(clients == 0) {
+    clearInterval(poll);
+  }
+  console.log('EXIT: number of clients ' + clients);
 })
 
 app.use(express.static('www'));
 
 app.listen(7076)
 
+var AWS = require('aws-sdk');
+AWS.config.region = 'us-east-1';
